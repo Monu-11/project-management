1a8999889e129694c7db94ed7de219d25d8d7a56
Monu-11 <monukumarsinghmonu@gmail.com>
2024-10-28 01:10:40 +0530

    image drom s3

--- client/src/components/common/SideBar/SideBar.tsx
+++ client/src/components/common/SideBar/SideBar.tsx
@@ -63,7 +63,12 @@ ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}
         </div>
         {/* TEAM */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
-          <Image src="/logo.png" alt="Logo" width={40} height={40} />
+          <Image
+            src="https://pm-mod-s3-images.s3.ap-south-1.amazonaws.com/logo.png"
+            alt="Logo"
+            width={40}
+            height={40}
+          />
           <div>
             <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
               EDROH TEAM
